8e799e4492a81fe32fabf10f7b903ab2263fa3d7
EC2 Default User <ec2-user@ip-172-31-62-28.ec2.internal>
2021-05-10 11:27:51 +0000

    [Finish]6-6応用課題⑥

--- bookers2/app/controllers/book_comments_controller.rb
+++ bookers2/app/controllers/book_comments_controller.rb
@@ -1,21 +1,27 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
+
+    @user =  @book.user
+    @book_comment = BookComment.new
+
     if comment.save
     flash[:notice] = "You have created comment successfully."
-    redirect_to book_path(book)
     else
       flash[:notice] = "空コメント不可"
-      redirect_to book_path(book)
+      redirect_to book_path(@book)
     end
   end
 
   def destroy
-    book_comment = BookComment.find(params[:id])
-    book_comment.destroy
-    redirect_back(fallback_location: root_path)
+    @book_comment = BookComment.find(params[:id])
+    @book = Book.find(@book_comment.book.id)
+    @user =  @book.user
+    @book_comment.destroy
+    @book_comment = BookComment.new
+
     flash[:notice] = "You have deleted comment successfully."
   end
 
